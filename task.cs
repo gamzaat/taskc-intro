2d225cc8d65b75bd8577309ef69becc529c5f274
gamzaat <gamzaat@code.edu.az>
2023-02-25 17:38:45 +0400

    taskc#intro

---
+++ task.cs
@@ -0,0 +1,67 @@
+﻿
+
+//#region task3
+
+//int[] array = { 9, 13, 17, 56, 81, 43 };
+//int enboyuk = array[0];
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (array[i] > enboyuk)
+//    {
+//        enboyuk = array[i];
+//    }
+
+//}
+//Console.WriteLine("en boyuk eded " + enboyuk + "dir");
+//#endregion
+
+//#region task2
+//int[] array = { -8, 10, -14, 17, 72 };
+//int positiv = 0;
+//int negativ = 0;
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (array[i] > 0)
+//    {
+//        positiv++;
+//    }
+//    else if (array[i] < 0)
+//    {
+//        negativ++;
+//    }
+//}
+//Console.WriteLine("musbet ededler" + positiv + "denedir");
+//Console.WriteLine("menfi ededler"+negativ+"denedir");
+
+//#endregion
+
+//#region task 1
+//Console.Write("imtahan qiymetini daxil edin ");
+//string numStr = Console.ReadLine();
+//    int number=Convert.ToInt32(numStr);
+
+//if (number >= 91 && number <= 100)
+//{
+//    Console.WriteLine("a");
+//}
+//else if (number >= 81 && number < 91)
+//{
+//    Console.WriteLine("b");
+//}
+//else if (number >= 71 && number < 81)
+//{
+//    Console.WriteLine("c");
+//}
+//else if (number >= 61 && number < 71)
+//{
+//    Console.WriteLine("d");
+//}
+//else if (number >= 51 && number < 61)
+//{
+//    Console.WriteLine("e");
+//}
+//else if (number < 51)
+//{
+//    Console.WriteLine ("kesildiniz ");
+//}
+//#endregion 
